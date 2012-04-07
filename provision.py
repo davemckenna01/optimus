d4cf3f3b422a37ef08764bb1aad276fbab5ba921
Dave McKenna <davemckenna01@gmail.com>
2012-04-07 01:02:13 -0400

    complete abstracting away game of thrones specific nouns and verbs

--- provision.py
+++ provision.py
@@ -2,13 +2,11 @@ import random
 import optimization
 import pprint
 
-consumers_labels = ['name', 'arrow weight', 'close combat weight']
 consumers = [
-  ['eastwatch', 0.2, 0.8],
-  ['castleblack', 0.4, 0.6],
-  ['shadowtower', 0.2, 0.8]
+  ['eastwatch'],
+  ['castleblack'],
+  ['shadowtower']
 ]
-
 #append an empty list to be filled by resources
 for consumer in consumers:
   consumer.append([]);
@@ -53,12 +51,9 @@ resources = [
 
 #What a solution looks like:
 # [0,1,2,0,2,0,1,0,0,2,1,2,0,0,1,0,2,1,1,0,2,1,0...]
-#where each list slot represents a particular brother
-#from the brothers list, and the value of a slot 
-#represents the castle that brother is assigned to
-
-#So, there are len(castles)**len(brothers) possibilities,
-#which at the time of writing = 3**34 = A SHITLOAD (like trillions)
+#where each list slot represents a particular resource
+#from the resources list, and the value of a slot 
+#represents the consumer that resource is assigned to
 
 #What the domain looks like
 # [(0,2),(0,2),(0,2),(0,2),(0,2),(0,2),(0,2)...]
@@ -73,7 +68,6 @@ randomvec = [random.randint(domain[i][0],domain[i][1])
 total_attr_strengths = []
 for a in range(0, len(resources[0]) - 1):
   total_attr_strengths.append(sum([r[a+1] for r in resources]))
-print total_attr_strengths
 
 #Ideal attribute strength at each consumer.
 #This is simple right now becuase we're just dividing total
@@ -81,7 +75,6 @@ print total_attr_strengths
 optimal_attr_strengths = []
 for a in range(0, len(total_attr_strengths)):
   optimal_attr_strengths.append(total_attr_strengths[a]/len(consumers))
-print optimal_attr_strengths
 
 def provisioning_cost(vec):
   cost = 0
@@ -97,7 +90,7 @@ def provisioning_cost(vec):
   #Loop through the consumers and determine how strength was 
   #distributed and calculate cost
   for c in range(len(consumers)):
-    consumer_resources = [resources[i] for i in consumers[c][len(consumers[0])-1]]
+    consumer_resources = [resources[res] for res in consumers[c][len(consumers[0])-1]]
 
     #Add up all the strength of the resources in the consumer
     solution_attr_strengths = []
@@ -106,12 +99,12 @@ def provisioning_cost(vec):
 
     #Normalize the number to be always non-negative: abs(). B/C whether 
     #the solution over or under supplies the consumer is irrelevant
-    for a in range(0, len(resources[0]) - 1):
+    for a in range(0, len(solution_attr_strengths)):
       cost += abs(optimal_attr_strengths[a] - solution_attr_strengths[a])
 
   return cost
 
-#Print out a solution (w/ names of brothers and castles, etc)
+#Print out a solution (w/ names of resources and consumers, etc)
 def print_solution(vec):
 
   #Make sure consumers are empty
@@ -124,12 +117,15 @@ def print_solution(vec):
 
   #Pretty print the solution
   for consumer in consumers:
-    print "=== Consumer: %s ===" % consumer[0]
+    print "=== Consumer: %s ===" % consumer[0] #the consumer "name"
     for res in consumer[len(consumers[0])-1]:
-      print "%15s A: %2s  S: %2s" % (resources[res][0], resources[res][1], resources[res][2])
+
+      for d in range(0, len(resources[0])):
+        print "%20s: %s" % (resources_labels[d], resources[res][d])
+
     for a in range(0, len(resources[0]) - 1):
-      total = sum([resources[i][a+1] for i in consumer[len(consumers[0])-1]])
-      print "%s strength at consumer: %s" % (resources_labels[a+1], total)
+      total = sum([resources[r][a+1] for r in consumer[len(consumers[0])-1]])
+      print "%s strength at %s: %s" % (resources_labels[a+1], consumer[0], total)
 
 #optimization.geneticoptimize(domain, defendcost)
 #optimization.annealingoptimize(domain, defendcost)
