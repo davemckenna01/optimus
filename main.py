8f96f85a65361264b7f8446fdbfe360f1d9a0576
Dave McKenna <davemckenna01@gmail.com>
2012-04-08 13:55:35 -0400

    more restful urls

--- main.py
+++ main.py
@@ -22,33 +22,28 @@ Run algorithm on file
 """
 
 class MainHandler(webapp.RequestHandler):
-    def get(self):
-        upload_url = blobstore.create_upload_url('/upload')
-        self.response.out.write('<html><body>')
-        self.response.out.write('<form action="%s" method="POST" enctype="multipart/form-data">' % upload_url)
-        self.response.out.write("""Upload File: <input type="file" name="file"><br> <input type="submit"
-            name="submit" value="Submit"> </form></body></html>""")
+  def get(self):
+    upload_url = blobstore.create_upload_url('/upload')
+    self.response.out.write('<html><body>')
+    self.response.out.write('<form action="%s" method="POST" enctype="multipart/form-data">' % upload_url)
+    self.response.out.write("""Upload File: <input type="file" name="file"><br> <input type="submit"
+                            name="submit" value="Submit"> </form></body></html>""")
+
 
 class UploadHandler(blobstore_handlers.BlobstoreUploadHandler):
   def post(self):
     upload_files = self.get_uploads('file')  # 'file' is file upload field in the form
     blob_info = upload_files[0]
-    
-    afile = Files(blobRef = blob_info.key(), name = blob_info.filename)
-    afile.put()
-
     self.redirect('/files')
-  
 
 
-class ServeHandler(blobstore_handlers.BlobstoreDownloadHandler):
+class FileDetailHandler(blobstore_handlers.BlobstoreDownloadHandler):
   def get(self, resource):
     resource = str(urllib.unquote(resource))
     blob_info = blobstore.BlobInfo.get(resource)
     self.send_blob(blob_info)
 
-
-class FileHandler(webapp.RequestHandler):
+class FileListHandler(webapp.RequestHandler):
   def get(self):
     all_files = blobstore.BlobInfo.all()
 
@@ -56,16 +51,15 @@ class FileHandler(webapp.RequestHandler):
 
     res = ''
     for f in all_files:
-      res += '<a href="/serve/%s">%s</a><br/>' % (f.key(), f.filename)
+      res += '<a href="/files/%s">%s</a><br/>' % (f.key(), f.filename)
 
     self.response.out.write(res)
 
 
-
 app = webapp2.WSGIApplication([('/', MainHandler),
                                ('/upload', UploadHandler),
-                               ('/serve/([^/]+)?', ServeHandler),
-                               ('/files', FileHandler),
+                               ('/files/([^/]+)?', FileDetailHandler),
+                               ('/files', FileListHandler),
                               ], debug=True)
 
 
