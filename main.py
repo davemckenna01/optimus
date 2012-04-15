cd330e85bcc5e95bf730e14486c1d5088d14a6c4
Dave McKenna <davemckenna01@gmail.com>
2012-04-15 01:21:11 -0400

    json versions of resources now available by .json

--- main.py
+++ main.py
@@ -24,7 +24,6 @@ class MainHandler(webapp.RequestHandler):
 class GetUploadURLHandler(webapp.RequestHandler):
   def get(self):
     upload_url = blobstore.create_upload_url('/files')
-
     self.response.out.write(json.dumps({'data':[{'uploadUrl': upload_url}]}))
 
 
@@ -37,14 +36,16 @@ class FileDetailHandler(blobstore_handlers.BlobstoreDownloadHandler):
 
 
 class FileHandler(blobstore_handlers.BlobstoreUploadHandler):
-  def post(self):
+  def post(self, mime):
     upload_files = self.get_uploads('file')  # 'file' is file upload field in the form
     blob_info = upload_files[0]
 
-    self.response.out.write(json.dumps({'data':[{'file': str(blob_info.key())}]}))
+    if mime == '.json':
+      self.response.out.write(json.dumps({'data':[{'file': str(blob_info.key())}]}))
+    else:
+      self.redirect('/files')
 
-
-  def get(self):
+  def get(self, mime):
     all_files = blobstore.BlobInfo.all()
 
     j = {'data':[]}
@@ -52,11 +53,14 @@ class FileHandler(blobstore_handlers.BlobstoreUploadHandler):
       url = '/files/%s' % f.key()
       j['data'].append({'name':f.filename, 'url': url})
 
-    self.response.out.write(json.dumps(j))
+    if mime == '.json':
+      self.response.out.write(json.dumps(j))
+    else:
+      self.response.out.write('<html><body><strong>' + json.dumps(j) + '</strong></body></html>')
 
 
 class FileOptimizeHandler(webapp.RequestHandler):
-  def get(self, blob_key, algorithm, consumers):
+  def get(self, blob_key, consumers, algorithm, mime):
 
     blob_reader = blobstore.BlobReader(blob_key)
 
@@ -69,14 +73,24 @@ class FileOptimizeHandler(webapp.RequestHandler):
 
     cost, sol_vec = prov.optimize()
 
-    self.response.out.write(json.dumps({'cost':cost,'solution':sol_vec}))
+    j = {'cost':cost,'solution':sol_vec}
+
+    if mime == '.json':
+      self.response.out.write(json.dumps(j))
+    else:
+      self.response.out.write('<html><body><strong>' + json.dumps(j) + '</strong></body></html>')
 
 
-app = webapp.WSGIApplication([('/', MainHandler),
+
+app = webapp.WSGIApplication([('/main', MainHandler),
                                ('/getUploadUrl', GetUploadURLHandler),
-                               ('/files/([^/]+)?/run/([^/]+)?/([^/]+)?', FileOptimizeHandler),
-                               ('/files/([^/]+)?', FileDetailHandler),
-                               ('/files', FileHandler),
+                               #I'm not crazy about this regex: (.+?)(\.[^.]*$|$)
+                               #that I'm using to pick out "file extensions" i.e. <resource>.json
+                               #or w/e. B/c this forces me to raise 404s on invalid "file extensions"
+                               #in the handler rather than by url matching... it's ok for now though
+                               ('/files/(.+?)/run/(.+?)/(.+?)(\.[^.]*$|$)', FileOptimizeHandler),
+                               ('/files/(.+?)', FileDetailHandler),
+                               ('/files(\.[^.]*$|$)', FileHandler),
                               ], debug=True)
 
 
